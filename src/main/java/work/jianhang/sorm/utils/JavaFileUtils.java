0edd4628abcf7c04b7b75a2f80f5a9fd37b8913f
JianhangLin <linjianhang1010@gmail.com>
2019-04-18 17:08:36 +0800

    增加根据表信息生成java类的源代码的逻辑

--- src/main/java/work/jianhang/sorm/utils/JavaFileUtils.java
+++ src/main/java/work/jianhang/sorm/utils/JavaFileUtils.java
@@ -2,7 +2,13 @@ package work.jianhang.sorm.utils;
 
 import work.jianhang.sorm.bean.ColumnInfo;
 import work.jianhang.sorm.bean.JavaFieldGetSet;
+import work.jianhang.sorm.bean.TableInfo;
+import work.jianhang.sorm.core.DBManager;
 import work.jianhang.sorm.core.TypeConvertor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 封装了生成Java文件(源代码)常用的操作
@@ -36,6 +42,48 @@ public class JavaFileUtils {
         setSrc.append("\t}\n");
         jfgs.setSetInfo(setSrc.toString());
         return jfgs;
+    }
+
+    /**
+     * 根据表信息生成java类的源代码
+     * @param tableInfo 表信息
+     * @param convertor 数据类型转换器
+     * @return java类的源代码
+     */
+    public static String createJavaSrc(TableInfo tableInfo, TypeConvertor convertor) {
+
+        Map<String, ColumnInfo> columns = tableInfo.getColumns();
+        List<JavaFieldGetSet> javaFields = new ArrayList<>();
+
+        for (ColumnInfo c : columns.values()) {
+            javaFields.add(createFieldGetSetSRC(c, convertor));
+        }
+
+        StringBuilder src = new StringBuilder();
+        // 生成package语句
+        src.append("package " + DBManager.getConf().getPoPackage() + ";\n\n");
+        // 生成import语句
+        src.append("import java.sql.*;\n");
+        src.append("import java.util.*;\n\n");
+        // 生成类声明语句
+        src.append("public class " + StringUtils.firstChar2UpperCase(tableInfo.getTname()) + " {\n\n");
+        // 生成属性列表
+        for (JavaFieldGetSet gs : javaFields) {
+            src.append(gs.getFieldInfo());
+        }
+        src.append("\n\n");
+        // 生成get方法列表
+        for (JavaFieldGetSet gs : javaFields) {
+            src.append(gs.getGetInfo());
+        }
+        // 生成set方法列表
+        for (JavaFieldGetSet gs : javaFields) {
+            src.append(gs.getSetInfo());
+        }
+        // 生成类结束符
+        src.append("}\n");
+        System.out.println(src);
+        return src.toString();
     }
 
 }
