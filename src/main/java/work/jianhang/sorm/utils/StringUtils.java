5cef0b4738e8b5d3b9981f71fe68c106c7864dac
JianhangLin <linjianhang1010@gmail.com>
2019-04-18 16:30:02 +0800

    增加将目标字符串首字母变为大写的工具类方法

--- src/main/java/work/jianhang/sorm/utils/StringUtils.java
+++ src/main/java/work/jianhang/sorm/utils/StringUtils.java
@@ -4,4 +4,14 @@ package work.jianhang.sorm.utils;
  * 封装了字符串常用的操作
  */
 public class StringUtils {
+
+    /**
+     * 将目标字符串首字母变为大写
+     * @param str 目标字符串
+     * @return 首字母变为大写的字符串
+     */
+    public String firstChar2UpperCase(String str) {
+        // abcd -->Abcd
+        return str.toUpperCase().substring(0, 1) + str.substring(1);
+    }
 }
