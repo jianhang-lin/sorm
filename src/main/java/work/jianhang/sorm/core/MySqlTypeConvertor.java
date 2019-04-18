f5f086423a09051aed5fd772c0c4f587c86a0634
JianhangLin <linjianhang1010@gmail.com>
2019-04-18 16:01:39 +0800

    实现mysql数据类型向java数据类型的转换

--- src/main/java/work/jianhang/sorm/core/MySqlTypeConvertor.java
+++ src/main/java/work/jianhang/sorm/core/MySqlTypeConvertor.java
@@ -6,6 +6,26 @@ package work.jianhang.sorm.core;
 public class MySqlTypeConvertor implements TypeConvertor {
 
     public String databaseType2JavaType(String columnType) {
+        if ("varchar".equalsIgnoreCase(columnType) || "char".equalsIgnoreCase(columnType)) {
+            return "String";
+        } else if ("int".equalsIgnoreCase(columnType) || "tinyint".equalsIgnoreCase(columnType)
+                    || "smallint".equalsIgnoreCase(columnType) || "integer".equalsIgnoreCase(columnType)) {
+            return "Integer";
+        } else if ("bigint".equalsIgnoreCase(columnType)) {
+            return "Long";
+        } else if ("double".equalsIgnoreCase(columnType) || "float".equalsIgnoreCase(columnType)) {
+            return "Double";
+        } else if ("clob".equalsIgnoreCase(columnType)) {
+            return "java.sql.Clob";
+        } else if ("blob".equalsIgnoreCase(columnType)) {
+            return "java.sql.Blob";
+        } else if ("date".equalsIgnoreCase(columnType)) {
+            return "java.sql.Date";
+        } else if ("time".equalsIgnoreCase(columnType)) {
+            return "java.sql.Time";
+        } else if ("timestamp".equalsIgnoreCase(columnType)) {
+            return "java.sql.Timestamp";
+        }
         return null;
     }
 
