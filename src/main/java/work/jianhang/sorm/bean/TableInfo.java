9abbd3ac610c975958ee420afc783a529f8d059e
JianhangLin <linjianhang1010@gmail.com>
2019-04-18 11:28:48 +0800

    定义TableInfo类

---
+++ src/main/java/work/jianhang/sorm/bean/TableInfo.java
@@ -0,0 +1,57 @@
+package work.jianhang.sorm.bean;
+
+import java.util.Map;
+
+/**
+ * 存储表结构的信息
+ */
+public class TableInfo {
+
+    /**
+     * 表名
+     */
+    private String tname;
+
+    /**
+     * 所有字段的信息
+     */
+    private Map<String, ColumnInfo> columns;
+
+    /**
+     * 唯一主键（目前只能处理表中有且只有一个主键的情况）
+     */
+    private ColumnInfo onlyPriKey;
+
+    public TableInfo() {
+    }
+
+    public TableInfo(String tname, Map<String, ColumnInfo> columns, ColumnInfo onlyPriKey) {
+        this.tname = tname;
+        this.columns = columns;
+        this.onlyPriKey = onlyPriKey;
+    }
+
+    public String getTname() {
+        return tname;
+    }
+
+    public void setTname(String tname) {
+        this.tname = tname;
+    }
+
+    public Map<String, ColumnInfo> getColumns() {
+        return columns;
+    }
+
+    public void setColumns(Map<String, ColumnInfo> columns) {
+        this.columns = columns;
+    }
+
+    public ColumnInfo getOnlyPriKey() {
+        return onlyPriKey;
+    }
+
+    public void setOnlyPriKey(ColumnInfo onlyPriKey) {
+        this.onlyPriKey = onlyPriKey;
+    }
+}
