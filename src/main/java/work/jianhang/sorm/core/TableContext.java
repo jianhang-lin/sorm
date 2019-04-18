40f27fa3d863cf2e423ee930e9481f64d0945de6
JianhangLin <linjianhang1010@gmail.com>
2019-04-18 15:43:16 +0800

    增加TableContext初始化时加载表信息

--- src/main/java/work/jianhang/sorm/core/TableContext.java
+++ src/main/java/work/jianhang/sorm/core/TableContext.java
@@ -1,7 +1,72 @@
 package work.jianhang.sorm.core;
 
+import work.jianhang.sorm.bean.ColumnInfo;
+import work.jianhang.sorm.bean.TableInfo;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 负责获取管理数据库所有表结构和类结构的关系，并可以根据表结构生成类结构
  */
 public class TableContext {
+    /**
+     * 表名为kye，表信息为value
+     */
+    public static Map<String, TableInfo> tables = new HashMap<String, TableInfo>();
+
+    /**
+     * 将po的class对象和表信息对象关联起来，便于重用
+     */
+    public static Map<Class, TableInfo> poClassTableMap = new HashMap<Class, TableInfo>();
+
+    public TableContext() {
+    }
+
+    static {
+        try {
+            // 初始化获取表的信息
+            Connection conn = DBManager.getConn();
+            DatabaseMetaData dbmd = conn.getMetaData();
+
+            ResultSet tableSet = dbmd.getTables(null, "%", "%", new String[]{"TABLE"});
+
+            while (tableSet.next()) {
+                String tableName = tableSet.getString("TABLE_NAME");
+                TableInfo ti = new TableInfo(tableName, new ArrayList<ColumnInfo>(), new HashMap<String, ColumnInfo>());
+                tables.put(tableName, ti);
+
+                // 查询表中的所有字段
+                ResultSet set = dbmd.getColumns(null, "%", tableName, "%");
+                while (set.next()) {
+                    ColumnInfo ci = new ColumnInfo(set.getString("COLUMN_NAME"), set.getString("TYPE_NAME"), 0);
+                    ti.getColumns().put(set.getString("COLUMN_NAME"), ci);
+                }
+
+                // 查询表中的主键
+                ResultSet set2 = dbmd.getPrimaryKeys(null, "%", tableName);
+                while (set2.next()) {
+                    ColumnInfo ci2 = ti.getColumns().get(set2.getObject("COLUMN_NAME"));
+                    ci2.setKeyType(1);// 设置主键类型
+                    ti.getPriKeys().add(ci2);
+                }
+                if (ti.getPriKeys().size() > 0) {
+                    ti.setOnlyPriKey(ti.getPriKeys().get(0));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static Map<String, TableInfo> getTableInfos() {
+        return tables;
+    }
+
 }
