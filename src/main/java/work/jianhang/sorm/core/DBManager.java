be96d6a426661fa71a5d48bc5ff88b94433bb8f8
JianhangLin <linjianhang1010@gmail.com>
2019-04-18 12:07:06 +0800

    实现维护连接对象的管理方法

--- src/main/java/work/jianhang/sorm/core/DBManager.java
+++ src/main/java/work/jianhang/sorm/core/DBManager.java
@@ -3,6 +3,7 @@ package work.jianhang.sorm.core;
 import work.jianhang.sorm.bean.Configuration;
 
 import java.io.IOException;
+import java.sql.*;
 import java.util.Properties;
 
 /**
@@ -27,5 +28,48 @@ public class DBManager {
         conf.setUsingDB(pros.getProperty("usingDB"));
         conf.setSrcPath(pros.getProperty("srcPath"));
         conf.setPoPackage(pros.getProperty("poPackage"));
+    }
+
+    public static Connection getConn() {
+        try {
+            Class.forName(conf.getDriver());
+            // TODO 直接建立连接，后期增加连接池处理，提高效率
+            return DriverManager.getConnection(conf.getUrl(), conf.getUser(), conf.getPwd());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void close(ResultSet rs, Statement ps, Connection conn) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (ps != null) {
+                ps.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void close(Statement ps, Connection conn) {
+        close(null, ps, conn);
+    }
+
+    public static void close(Connection conn) {
+        close(null, null, conn);
     }
 }
