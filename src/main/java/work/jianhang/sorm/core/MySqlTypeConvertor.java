7acc646ebd64aeeeb89ea89a18b2041caa8d6633
JianhangLin <linjianhang1010@gmail.com>
2019-04-18 15:54:21 +0800

    增加Mysql类型和java数据类型转换的实现类

---
+++ src/main/java/work/jianhang/sorm/core/MySqlTypeConvertor.java
@@ -0,0 +1,15 @@
+package work.jianhang.sorm.core;
+
+/**
+ * mysql数据库类型和java数据类型的转换
+ */
+public class MySqlTypeConvertor implements TypeConvertor {
+
+    public String databaseType2JavaType(String columnType) {
+        return null;
+    }
+
+    public String javaType2DatabaseType(String javaDataType) {
+        return null;
+    }
+}
