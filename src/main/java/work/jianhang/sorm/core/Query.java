a55283eebcb197b68954538cbd72ed92c150dc01
JianhangLin <linjianhang1010@gmail.com>
2019-04-19 22:04:38 +0800

    优化代码，提取公共方法代码至父类

--- src/main/java/work/jianhang/sorm/core/Query.java
+++ src/main/java/work/jianhang/sorm/core/Query.java
@@ -1,11 +1,19 @@
 package work.jianhang.sorm.core;
 
+import work.jianhang.sorm.bean.ColumnInfo;
+import work.jianhang.sorm.bean.TableInfo;
+import work.jianhang.sorm.utils.JDBCUtils;
+import work.jianhang.sorm.utils.ReflectUtils;
+
+import java.lang.reflect.Field;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 负责查询（对外提供服务的核心类）
  */
-public interface Query {
+public abstract class Query {
 
     /**
      * 直接执行一个DML语句
@@ -13,26 +21,91 @@ public interface Query {
      * @param params 参数
      * @return 执行sql语句后影响记录的行数
      */
-    public int executeDML(String sql, Object[] params);
+    public int executeDML(String sql, Object[] params) {
+        Connection conn = DBManager.getConn();
+        int count = 0;
+        PreparedStatement ps = null;
+
+        try {
+            ps = conn.prepareStatement(sql);
+
+            // 给sql设值
+            JDBCUtils.handleParams(ps, params);
+
+            count = ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBManager.close(ps, conn);
+        }
+        return count;
+    }
 
     /**
      * 将一个对象存储到数据库中，把对象中不为null的属性往数据库存储，如果数字为null则为0。
      * @param obj 要存储的对象
      */
-    public void insert(Object obj);
+    public void insert(Object obj) {
+        // obj-->表中。 insert into 表名(id, name, pwd) values (?, ?, ?)
+        Class c = obj.getClass();
+        List<Object> params = new ArrayList<>(); // 存储sql参数对象
+        TableInfo tableInfo = TableContext.poClassTableMap.get(c);
+        StringBuilder sql = new StringBuilder("insert into ");
+        sql.append(tableInfo.getTname());
+        sql.append(" (");
+        int countNotNullField = 0; // 计算不为null的属性值
+        Field[] fs = c.getDeclaredFields();
+        for (Field f : fs) {
+            String fieldName = f.getName();
+            Object fieldValue = ReflectUtils.invokeGet(fieldName, obj);
+
+            if (fieldValue != null) {
+                countNotNullField++;
+                sql.append(fieldName);
+                sql.append(",");
+                params.add(fieldValue);
+            }
+        }
+
+        sql.setCharAt(sql.length() - 1, ')');
+        sql.append(" values (");
+        for (int i=0; i<countNotNullField; i++) {
+            sql.append("?,");
+        }
+        sql.setCharAt(sql.length() - 1, ')');
+
+        executeDML(sql.toString(), params.toArray());
+    }
 
     /**
      * 删除clazz表示类对应的表中的记录(指定主键值id的记录)
      * @param clazz 跟表对应的类的Class对象
      * @param id 主键的值
      */
-    public void delete(Class clazz, Object id); // delete form User where id = 2
+    public void delete(Class clazz, Object id) {
+        // Emp.class,2-->delete from emp where id = 2
+        // 通过Class对象找TableInfo
+        TableInfo tableInfo = TableContext.poClassTableMap.get(clazz);
+        // 获得主键
+        ColumnInfo onlyPriKey = tableInfo.getOnlyPriKey();
+
+        String sql = "delete from " + tableInfo.getTname() + " where " + onlyPriKey.getName() + " = ? ";
+        executeDML(sql, new Object[]{id});
+    }
 
     /**
      * 删除对象在数据库中对应的记录(对象所在的类对应列表，对象的主键的值对应到记录)
      * @param obj
      */
-    public void delete(Object obj);
+    public void delete(Object obj) {
+        Class c = obj.getClass();
+        TableInfo tableInfo = TableContext.poClassTableMap.get(c);
+        ColumnInfo onlyPriKey = tableInfo.getOnlyPriKey();
+
+        // 通过反射机制，调用属性对应的get方法或set方法
+        Object priKeyValue = ReflectUtils.invokeGet(onlyPriKey.getName(), obj);
+        delete(c, priKeyValue);
+    }
 
     /**
      * 更新对象对应的记录，并且只更新指定的字段的值
@@ -40,7 +113,26 @@ public interface Query {
      * @param fieldNames 更新的属性列表
      * @return 执行sql语句后影响记录的行数
      */
-    public int update(Object obj, String[] fieldNames); // update user set uname = ?, pwd = ?
+    public int update(Object obj, String[] fieldNames) {
+        // obj{"username", "pwd"} --> update 表名 set username = ?, pwd = ? where id = ?
+        Class c = obj.getClass();
+        List<Object> params = new ArrayList<>(); // 存储sql参数对象
+        TableInfo tableInfo = TableContext.poClassTableMap.get(c);
+        ColumnInfo priKey = tableInfo.getOnlyPriKey();
+        StringBuilder sql = new StringBuilder("update ");
+        sql.append(tableInfo.getTname()).append(" set ");
+
+        for (String fname : fieldNames) {
+            Object fvalue = ReflectUtils.invokeGet(fname, obj);
+            params.add(fvalue);
+            sql.append(fname).append(" = ?,");
+        }
+        sql.setCharAt(sql.length() - 1, ' ');
+        sql.append(" where ").append(priKey.getName()).append(" = ?");
+
+        params.add(ReflectUtils.invokeGet(priKey.getName(), obj));
+        return executeDML(sql.toString(), params.toArray());
+    }
 
     /**
      * 查询返回多行记录，并将每行记录封装到clazz指定的类的对象中
@@ -49,7 +141,46 @@ public interface Query {
      * @param params sql参数
      * @return 查询到的结果
      */
-    public List queryRows(String sql, Class clazz, Object[] params);
+    public List queryRows(String sql, Class clazz, Object[] params) {
+        Connection conn = DBManager.getConn();
+        List list = null; // 存储查询结果的容器
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            ps = conn.prepareStatement(sql);
+
+            // 给sql设值
+            JDBCUtils.handleParams(ps, params);
+            rs = ps.executeQuery();
+            ResultSetMetaData metaData = rs.getMetaData();
+            // 多行
+            while (rs.next()) {
+                if (list == null) {
+                    list = new ArrayList();
+                }
+                Object rowObject = clazz.newInstance(); // 调用javabean的无参构造器
+
+                // 多列 select empname, age from emp where id > ? and age > 18
+                for (int i=0; i<metaData.getColumnCount(); i++) {
+                    String columnName = metaData.getColumnLabel(i+1);
+                    Object columnValue = rs.getObject(i+1);
+
+                    // 调用rowObj对象的setXXX(String xxx)方法，将columnValue的值设置进去
+                    ReflectUtils.invokeSet(rowObject, columnName, columnValue);
+                }
+                list.add(rowObject);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } finally {
+            DBManager.close(ps, conn);
+        }
+        return list;
+    }
 
     /**
      * 查询返回一行记录，并将该记录封装到clazz指定的类的对象中
@@ -58,7 +189,10 @@ public interface Query {
      * @param params sql参数
      * @return 查询到的结果
      */
-    public Object queryUniqueRows(String sql, Class clazz, Object[] params);
+    public Object queryUniqueRows(String sql, Class clazz, Object[] params) {
+        List list = queryRows(sql, clazz, params);
+        return (list != null && list.size() > 0)?list.get(0) : null;
+    }
 
     /**
      * 查询返回一个值(一行一列)，并将该值返回
@@ -66,7 +200,28 @@ public interface Query {
      * @param params sql参数
      * @return 查询到的结果
      */
-    public Object queryValue(String sql, Object[] params);
+    public Object queryValue(String sql, Object[] params) {
+        Connection conn = DBManager.getConn();
+        Object value = null; // 存储查询结果的对象
+        PreparedStatement ps = null;
+        ResultSet rs;
+        try {
+            ps = conn.prepareStatement(sql);
+
+            // 给sql设值
+            JDBCUtils.handleParams(ps, params);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                // select count(*) from emp
+                value = rs.getObject(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBManager.close(ps, conn);
+        }
+        return value;
+    }
 
     /**
      * 查询返回一个数字(一行一列)，并将该值返回
@@ -74,5 +229,7 @@ public interface Query {
      * @param params sql参数
      * @return 查询到的数字
      */
-    public Number queryNumber(String sql, Object[] params);
+    public Number queryNumber(String sql, Object[] params) {
+        return (Number) queryValue(sql, params);
+    }
 }
