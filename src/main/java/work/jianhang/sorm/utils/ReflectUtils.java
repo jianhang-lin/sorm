8061108c2e570d2d46a6f7ee17ed56338d9fc455
JianhangLin <linjianhang1010@gmail.com>
2019-04-19 09:23:44 +0800

    增加调用obj对象对应属性fieldName的get方法的逻辑

--- src/main/java/work/jianhang/sorm/utils/ReflectUtils.java
+++ src/main/java/work/jianhang/sorm/utils/ReflectUtils.java
@@ -1,7 +1,33 @@
 package work.jianhang.sorm.utils;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
  * 封装了反射常用的操作
  */
 public class ReflectUtils {
+
+    /**
+     * 调用obj对象对应属性fieldName的get方法
+     * @param fieldName
+     * @param obj
+     * @return
+     */
+    public static Object invokeGet(String fieldName, Object obj) {
+        try {
+            Class c = obj.getClass();
+            Method m = c.getMethod("get" + StringUtils.firstChar2UpperCase(fieldName), null);
+            return m.invoke(obj, null);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            return null;
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+            return null;
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
