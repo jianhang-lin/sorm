93ba2b72a61e097340d76be80fdd0db2363ee4a4
JianhangLin <linjianhang1010@gmail.com>
2019-04-19 23:36:30 +0800

    使用克隆方式实现调用创建Query对象的工厂类的方法

--- src/main/java/work/jianhang/sorm/core/QueryFactory.java
+++ src/main/java/work/jianhang/sorm/core/QueryFactory.java
@@ -1,7 +1,36 @@
 package work.jianhang.sorm.core;
 
+/**
+ * 创建Query对象的工厂类
+ */
 public class QueryFactory {
 
-//    public Query createQuery();
+    private static QueryFactory factory = new QueryFactory();
+    private static Query prototypeObj; // 原型对象
+
+    static {
+        try {
+            Class c = Class.forName(DBManager.getConf().getQueryClass());
+            prototypeObj = (Query) c.newInstance();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private QueryFactory() {
+    }
+
+    public Query createQuery() {
+        try {
+            return (Query) prototypeObj.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
 }
