3695a9ced8bf66f4c9a7da445b31e06fe458f3a8
JianhangLin <linjianhang1010@gmail.com>
2019-04-20 12:02:35 +0800

    抽取公共方法

--- src/main/java/work/jianhang/test/Test.java
+++ src/main/java/work/jianhang/test/Test.java
@@ -10,7 +10,12 @@ import java.util.List;
  * 客户端调用的测试类
  */
 public class Test {
+
     public static void main(String[] args) {
+        test01();
+    }
+
+    public static void test01() {
         Query q = QueryFactory.createQuery();
         String sql2 = "select e.id, e.empname, 2 * salary 'xinshui', age, d.dname 'deptName', d.address 'deptAddr' from emp e join dept d on e.deptId = d.id";
         List<EmpVO> empVOs = q.queryRows(sql2, EmpVO.class, null);
