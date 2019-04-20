09711fcd0a24aad326f9af68c1ba567ead5a940d
JianhangLin <linjianhang1010@gmail.com>
2019-04-20 10:38:20 +0800

    新增客户端调用的测试类

---
+++ src/main/java/work/jianhang/test/Test.java
@@ -0,0 +1,21 @@
+package work.jianhang.test;
+
+import work.jianhang.sorm.core.Query;
+import work.jianhang.sorm.core.QueryFactory;
+import work.jianhang.vo.EmpVO;
+
+import java.util.List;
+
+/**
+ * 客户端调用的测试类
+ */
+public class Test {
+    public static void main(String[] args) {
+        Query q = QueryFactory.createQuery();
+        String sql2 = "select e.id, e.empname, 2 * salary 'xinshui', age, d.dname 'deptName', d.address 'deptAddr' from emp e join dept d on e.deptId = d.id";
+        List<EmpVO> empVOs = q.queryRows(sql2, EmpVO.class, null);
+        for (EmpVO vo : empVOs) {
+            System.out.println(vo.getId() + ":" + vo.getEmpname() + ":" + vo.getXinshui() + ":" + vo.getAge() + ":" + vo.getDeptName() + ":" + vo.getDeptAddr());
+        }
+    }
+}
